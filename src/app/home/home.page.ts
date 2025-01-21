0d7517974cef25bf791df56af9a4e10f607ebc17
gabriel250206 <158100593+gabriel250206@users.noreply.github.com>
2025-01-21 10:23:41 -0600

    primerCommit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +9,14 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+
+  items: {id:number;name:string;date:string; completed:boolean}[]=[]
+  nextId:number=1
+
   constructor() {}
+
+  addItem(name:string,date:string):void{
+    
+  }
+
 }
